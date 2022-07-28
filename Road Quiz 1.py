85948dd5d163a23defa69b2fcfa495ba7f029d6e
YahiaKatory23 <103226045+YahiaKatory23@users.noreply.github.com>
2022-07-29 09:35:12 +1200

    Add files via upload

---
+++ Road Quiz 1.py
@@ -0,0 +1,18 @@
+print("Welcome to Yahia's Driving Quiz!")
+Start = input(print("Would you like to start? Yes/No "))
+print("To answer, Type either '1', '2', '3' ")
+Q1 = ("Question 1: On what side of the road do you drive in? Option 1: The left side")
+Q2 = ("Question 2: ")
+Q3 = ("Question 3: ")
+Q4 = ("Question 4: ")
+Q5 = ("Question 5: ")
+Q6 = ("Question 6: ")
+Q7 = ("Question 7: ")
+Q8 = ("Question 8: ")
+Q9 = ("Question 9: ")
+Q10 = ("Question 10: ")
+if Start = "Yes":
+    input(print(Q1))
+elif Start = "No":
+    Start = (print("Then Dont"))
+if 
